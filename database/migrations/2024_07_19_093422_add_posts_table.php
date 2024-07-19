bbc6dc041563173e42054794236d224655cb3777
Gusti Suparta <gusti.tut@gmail.com>
2024-07-20 05:50:19 +0800

    latihan nambahin post seeder

--- database/migrations/2024_07_19_093422_add_posts_table.php
+++ database/migrations/2024_07_19_093422_add_posts_table.php
@@ -11,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->text('description');
-            $table->boolean('status');
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+            $table->date('published_date');
+            $table->integer('user_id');
         });
     }
 
@@ -25,6 +22,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
     }
 };
